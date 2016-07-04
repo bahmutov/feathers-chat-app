f3b7f0f9b7db5c856d5f671fcac7870753b13971
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-07-04 14:29:44 -0400

    messages

--- no-deps.js
+++ no-deps.js
@@ -3,3 +3,4 @@ const pkg = require('./package.json')
 pkg.dependencies = pkg.devDependencies = []
 const write = require('fs').writeFileSync
 write('./package.json', JSON.stringify(pkg, null, 2) + '\n', 'utf8')
+console.log('removed dependencies from package.json')
